8571c2b3850905bf73eb20750c6508450905c920
Matthew Baer <matt.baer77@gmail.com>
2023-12-11 23:25:38 -0600

    completed create-account.js - fixed spelling errors in tests

--- create-account.js
+++ create-account.js
@@ -1,4 +1,55 @@
-function createAccount(pin, amount) {
+function createAccount(pin, amount = 0) {
+
+    const account = {
+
+        checkBalance: (enteredPin)=>{
+            if(enteredPin === pin){
+                return `$${amount}`;
+            }
+            return "Invalid PIN."
+        },
+        deposit: (enteredPin, depositAmount)=>{
+
+            if(enteredPin === pin){
+
+                amount = amount + depositAmount;
+                return `Successfully deposited $${depositAmount}. Current balance: $${amount}.`
+
+            }
+
+            return "Invalid PIN."
+
+        },
+        withdraw: (enteredPin, withdrawAmount)=>{
+
+            if(enteredPin === pin){
+
+                if(withdrawAmount > amount){
+                    return "Withdrawal amount exceeds account balance. Transaction cancelled."
+                }
+
+                amount = amount - withdrawAmount;
+
+                return `Successfully withdrew $${withdrawAmount}. Current balance: $${amount}.`
+
+            }
+
+            return "Invalid PIN."
+
+        },
+        changePin: (enteredPin, newPin)=>{
+            if(enteredPin === pin){
+
+                pin = newPin;
+                return "PIN successfully changed!"
+
+            }
+            return "Invalid PIN."
+        }
+
+    }
+
+    return account;
 
 }
 
