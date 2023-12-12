fc0b43761ef4ca7a5dc2b5942e0de50d451f814c
Matthew Baer <matt.baer77@gmail.com>
2023-12-11 23:00:58 -0600

    completed guessing-game

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,34 @@
 function guessingGame() {
 
+    let count = 0;
+    let guessed = false;
+    const secretNumber = Math.floor(Math.random() * 100);
+
+    const game = (guess) => {
+
+        count ++
+
+        if(guessed){
+            return "The game is over, you already won!"
+        }
+
+        if(guess === secretNumber){
+            guessed = true;
+            return `You win! You found ${guess} in ${count} guesses.`
+        }
+
+        if(guess > secretNumber){
+            return `${guess} is too high!`
+        }
+
+        if(guess < secretNumber){
+            return `${guess} is too low!`
+        }
+
+    }
+
+    return game;
+
 }
 
 module.exports = { guessingGame };
